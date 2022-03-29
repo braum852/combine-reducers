c6b336de4bfac20f8f67ce4649b6c889db2037aa
braum852 <julian.teo59@yahoo.com>
2022-03-29 12:38:16 +1000

    Done

--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -1,42 +1,41 @@
-export default function bookApp(
-  state = {
-    authors: [],
-    books: []
-  },
-  action
-) {
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+  authors: authorsReducer,
+  books: booksReducer
+});
+
+export default rootReducer;
+
+function booksReducer(state = [], action) {
   let idx;
   switch (action.type) {
     case "ADD_BOOK":
-      return {
-        ...state,
-        authors: [...state.authors],
-        books: [...state.books, action.book]
-      };
- 
+      return [...state, action.book];
+
     case "REMOVE_BOOK":
-      idx = state.books.findIndex(book => book.id === action.id);
+      idx = state.findIndex(book => book.id  === action.id)
       return {
-        ...state,
-        authors: [...state.authors],
-        books: [...state.books.slice(0, idx), ...state.books.slice(idx + 1)]
-      };
- 
+        books: state.book.filter(book => book !== idx)
+      }
+
+    default:
+      return state;
+  }
+}
+
+function authorsReducer(state = [], action) {
+  let idx;
+  switch (action.type) {
     case "ADD_AUTHOR":
-      return {
-        ...state,
-        books: [...state.books],
-        authors: [...state.authors, action.author]
-      };
- 
+      return [...state, action.author];
+
     case "REMOVE_AUTHOR":
-      idx = state.authors.findIndex(author => author.id === action.id);
+      idx = state.findIndex(author => author.id  === action.id)
       return {
-        ...state,
-        books: [...state.books],
-        authors: [...state.authors.slice(0, idx), ...state.authors.slice(idx + 1)]
+        authors: state.author.filter(author => author !== idx)
       };
- 
+
     default:
       return state;
   }
